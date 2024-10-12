c465ad4345fca00258afb4be1bffe5e7a294e5be
Miguel Ballesteros <git@miballe.dev>
2024-10-12 10:44:21 -0500

    Added generic avro file read method

--- src/factiva/analytics/integration/files.py
+++ src/factiva/analytics/integration/files.py
@@ -7,7 +7,7 @@ from ..common import const
 class SnapshotFiles(object):
 
 
-    def read_file(self, filepath, only_stats=False, merge_body=False) -> pd.DataFrame:
+    def read_avro_file(self, filepath, only_stats=False, merge_body=False) -> pd.DataFrame:
         """Reads a single Dow Jones snapshot datafile
         Parameters
         ----------
@@ -49,7 +49,7 @@ class SnapshotFiles(object):
         return r_df
 
 
-    def read_folder(self, folderpath, file_format='AVRO', only_stats=False, merge_body=False) -> pd.DataFrame:
+    def read_avro_folder(self, folderpath, file_format='AVRO', only_stats=False, merge_body=False) -> pd.DataFrame:
         """Scans a folder and reads the content of all files matching the format (file_format)
         Parameters
         ----------
@@ -72,6 +72,24 @@ class SnapshotFiles(object):
         r_df = pd.DataFrame()
         for filename in os.listdir(folderpath):
             if filename.lower().endswith("." + format_suffix):
-                t_df = self.read_file(folderpath + "/" + filename, only_stats, merge_body)
+                t_df = self.read_avro_file(folderpath + "/" + filename, only_stats, merge_body)
                 r_df = pd.concat([r_df, t_df])
+        return r_df
+
+    def read_raw_avro(self, filepath) -> pd.DataFrame:
+        """Reads a generic AVRO file into a Pandas DataFrame
+        Parameters
+        ----------
+        filepath : str
+            Relative or absolute file path
+        Returns
+        -------
+        pandas.DataFrame
+            A single Pandas Dataframe with the file content
+        """
+        with open(filepath, "rb") as fp:
+            reader = fastavro.reader(fp)
+            records = [r for r in reader]
+            r_df = pd.DataFrame.from_records(records)
+
         return r_df
